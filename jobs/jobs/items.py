1db7b451b5e041e097e55b35272d2cc9a8bdb911
hcf32 <bygeoffreyouma@gmail.com>
2024-09-03 12:26:05 +0300

    Start coding

--- jobs/jobs/items.py
+++ jobs/jobs/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class JobsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    recrtr_name = scrapy.Field()
+    recrtr_email = scrapy.Field()
